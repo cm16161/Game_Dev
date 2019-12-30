4e0700684628694bc81431d0de34431c3d7b8ae5
Chetan Mistry <cmistry98@gmail.com>
2019-12-30 18:27:11 +0000

    Draw Text to screen
    Signed-off-by: Chetan Mistry <cmistry98@gmail.com>

--- helloworld/helloworld/src/main.rs
+++ helloworld/helloworld/src/main.rs
@@ -1,76 +1,43 @@
-extern crate glutin_window;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate piston;
+extern crate piston_window;
 
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
-use piston::window::WindowSettings;
+use piston_window::*;
 
-pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64,  // Rotation for the square.
+const WINDOW_WIDTH: f64 = 1920_f64;
+const WINDOW_HEIGHT: f64 = 200_f64;
+
+fn get_center(string: &str, glyphs: &mut Glyphs) -> f64 {
+    use piston_window::character::CharacterCache;
+    let w = glyphs.width(32, string).unwrap();
+    return (WINDOW_WIDTH - w) / 2.0;
 }
 
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
+fn print_text(glyphs: &mut Glyphs, string: &str, size: u32, c: &Context, g: &mut G2d) {
+    let transform = c
+        .transform
+        .trans(get_center(string, glyphs), WINDOW_HEIGHT / 2.0);
 
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+    
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
-
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(GREEN, gl);
-
-            let transform = c
-                .transform
-                .trans(x, y)
-                .rot_rad(rotation)
-                .trans(-25.0, -25.0);
-
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
-        });
-    }
-
-    fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
-    }
+    text::Text::new(size)
+        .draw(string, glyphs, &c.draw_state, transform, g)
+        .unwrap();
 }
 
 fn main() {
-    // Change this to OpenGL::V2_1 if not working.
-    let opengl = OpenGL::V3_2;
+    let mut window: PistonWindow =
+        WindowSettings::new("Hello World", [WINDOW_WIDTH, WINDOW_HEIGHT])
+            .exit_on_esc(true)
+            .build()
+            .unwrap();
 
-    // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("spinning-square", [200, 200])
-        .graphics_api(opengl)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
+    let mut glyphs = window.load_font("assets/FiraSans-Regular.ttf").unwrap();
+    window.set_lazy(true);
 
-    // Create a new game and run it.
-    let mut app = App {
-        gl: GlGraphics::new(opengl),
-        rotation: 0.0,
-    };
-
-    let mut events = Events::new(EventSettings::new());
-    while let Some(e) = events.next(&mut window) {
-        if let Some(args) = e.render_args() {
-            app.render(&args);
-        }
-
-        if let Some(args) = e.update_args() {
-            app.update(&args);
-        }
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g, device| {
+            clear([0.0, 1.0, 1.0, 1.0], g);
+            print_text(&mut glyphs, "World!", 32, &c, g);
+            glyphs.factory.encoder.flush(device);
+        });
     }
 }
